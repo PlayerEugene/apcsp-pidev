15f76f8bff46accde2778b4d3c4096d3774c1b98
Eugene Lee <leee8134@my.sduhsd.net>
2019-05-23 15:46:45 +0000

    pointers

---
+++ cdemo/pointers.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+  
+  int a;
+  int* ptrtoa;
+
+  ptrtoa = &a;
+
+  a = 5;
+  printf("The value of a is %d\n", a);
+
+  *ptrtoa = 6;
+  printf("The value of a is %d\n", a);
+
+  printf("The value of ptrtoa is %d\n", ptrtoa);
+  printf("It stores the value %d\n", *ptrtoa);
+  printf("The address of a is %d\n", &a);
+
+  float d = 1.5;
+  float e = 2.5;
+  printf("The address of d is %d\n", &d);
+  printf("The address of e is %d\n", &e);
+  float* ad = &d;
+  float* ae = &e;
+  float tmp;
+  tmp = d;
+  d = e;
+  e = tmp;
+}
