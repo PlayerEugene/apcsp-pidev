1ca32c37b83038b376f904878bfc86f836a8b6dc
Eugene Lee <leee8134@my.sduhsd.net>
2019-05-24 15:09:43 +0000

    area

---
+++ cdemo/area.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+float areaOfCircle(float r)
+{
+        float area = 3.14*r*r;
+        return area;
+}
+
+int main()
+{
+	float a = 3.5;
+	while (a < 12.5)
+	{
+	float r = areaOfCircle(a);
+	printf("The area of a circle with radius %f is %f\n", a, r);
+	a++;
+	}
+}
